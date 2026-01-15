fb228a4b594aca1872c920de656924c2a9749803
JeontaRoyTurjo <jeontaroy@gmail.com>
2026-01-15 17:29:48 +0600

    Added University Dashboard UI

--- views/dashboard/university.php
+++ views/dashboard/university.php
@@ -1,5 +1,17 @@
 <?php include 'views/layouts/header.php'; ?>
-<h1>Welcome University</h1>
-<p>This is the university dashboard.</p>
-<a href="index.php?url=logout">Logout</a>
+
+<center>
+    <h1>Welcome, <?php echo $_SESSION['username']; ?></h1>
+    
+    <h3>University Dashboard</h3>
+    
+    <a href="index.php?url=university_profile">University Profile</a> <br><br>
+    <a href="index.php?url=manage_courses">Manage Courses</a> <br><br>
+    <a href="index.php?url=view_applications">View Applications</a> <br><br>
+    
+    <hr>
+    
+    <a href="index.php?url=logout">Logout</a>
+</center>
+
 <?php include 'views/layouts/footer.php'; ?>
